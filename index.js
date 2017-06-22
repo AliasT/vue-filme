807e8e09b1cd0020f21f1fd9d0ebef99a0260fa7
xiaobing <1481569358@qq.com>
2017-06-22 15:38:45 +0800

    Update index.js

--- index.js
+++ index.js
@@ -14,24 +14,26 @@ Vue.directive('fixme', {
     // 当前仅适用于window层的滚动
     function windowScroll (e) {
       const { top, height} = el.getBoundingClientRect()
-      const child = el.children[0]
-      if(!child) return
       const offsetTop = el._top - document.body.scrollTop
+      
       if(offsetTop <= 0 && !el.fixme) {
+        // 拷贝一份元素
         el._cloned = el.cloneNode(true)
         el._cloned.style.opacity = 0
         el._cloned.id = "cloned_" + Vue._FIXME_UUID++
         el.parentNode.appendChild(el._cloned)
+        
         el.fixme = true
-        const rect = child.getBoundingClientRect()
-        el.style.height = rect.height + 'px'
+        const rect = el._cloned.getBoundingClientRect()
         // 复刻元素当前可能影响样式的所有元素
+        // 固定样式
         setStyle(el, {
           position: 'fixed',
           width: rect.width + 'px',
           left: rect.left + 'px',
           top: 0,
-          opacity: 0.9
+          opacity: 0.9,
+          zIndex: 2000
         })
       } else if (offsetTop > 0) {
         if(el._cloned) {
