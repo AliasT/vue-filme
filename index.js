17fc1605de81ed3251a29ec550d7795a653f70d1
xiaobing <1481569358@qq.com>
2017-06-23 16:45:20 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-Vue._FIXME_UUID = 0
 
 function setStyle(target, props) {
   Object.keys(props).reduce((ret, cur) => {
@@ -7,43 +6,50 @@ function setStyle(target, props) {
   }, target)
 }
 
+Vue._FIXME_UUID = 0
+
 Vue.directive('fixme', {
   inserted(el) {
-    window.onscroll = windowScroll
     el._top = el.getBoundingClientRect().top
     // 当前仅适用于window层的滚动
-    function windowScroll (e) {
+    // 晃动是因为变量共享？
+    el.windowScroll = function (e) {
       const { top, height} = el.getBoundingClientRect()
       const offsetTop = el._top - document.body.scrollTop
-      
-      if(offsetTop <= 0 && !el.fixme) {
-        // 拷贝一份元素
-        el._cloned = el.cloneNode(true)
-        el._cloned.style.opacity = 0
-        el._cloned.id = "cloned_" + Vue._FIXME_UUID++
-        el.parentNode.appendChild(el._cloned)
-        
-        el.fixme = true
-        const rect = el._cloned.getBoundingClientRect()
-        // 复刻元素当前可能影响样式的所有元素
-        // 固定样式
-        setStyle(el, {
-          position: 'fixed',
-          width: rect.width + 'px',
-          left: rect.left + 'px',
-          top: 0,
-          opacity: 0.9,
-          zIndex: 2000
-        })
-      } else if (offsetTop > 0) {
-        if(el._cloned) {
-          el.parentNode.removeChild(document.getElementById(el._cloned.id))
-          el._cloned = null
-        }
-        setStyle(el, { position: 'initial' })
 
-        el.fixme = false
+      if(offsetTop <= 0 && !el.timeout) {
+        el.timeout = setTimeout(() => {
+          el._cloned = el.cloneNode(true)
+          el.parentNode.insertBefore(el._cloned, el)
+          el._cloned.style.opacity = 0
+          el._cloned.id = "cloned_" + Vue._FIXME_UUID++
+          el.style.top = top + 'px'
+          const rect = el._cloned.getBoundingClientRect()
+          // 复刻元素当前可能影响样式的所有元素
+          setStyle(el, {
+            transition: 'top .2s ease-out',
+            position: 'fixed',
+            width: rect.width + 'px',
+            left: rect.left + 'px',
+            top: 0,
+            opacity: 0.9,
+            zIndex: 2000
+          })
+          el.fixme = true
+        }, 100)
+      } else if (offsetTop > 0) {
+        if(el.timeout) {
+          el.parentNode.removeChild(document.getElementById(el._cloned.id))
+          setStyle(el, { position: 'initial', transition: '' })
+          clearTimeout(el.timeout)
+          el.timeout = null
+          el._cloned = null
+        }        
       }
     }
+    window.addEventListener('scroll', el.windowScroll)
+  },
+  unbind(el) {
+    window.removeEventListener('scroll', el.windowScroll)
   }
 })
